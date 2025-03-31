9d5c240d323918178f58620991d844ea56992955
Marjovic <marjovicalejado123.com>
2025-03-31 13:12:01 +0800

    Add database configuration file

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'library_managementdb';
+    $username = 'root';
+    $password = 'Sheamar@442211';
+
+    try {
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch(PDOException $e) {
+        die(json_encode([
+            'status' => 'error',
+            'message' => 'Connection failed: ' . $e->getMessage()
+        ]));
+    }
+?>
